faeb5bdd4074b1ec1438bb63fc43869af359593a
PRicoSaint <santosesteban09@gmail.com>
2021-08-02 02:39:20 -0500

    Updated the models to match what was needed and updated each of the routes. Ran it and see some of it work. Still getting some sequelize errors.

--- models/index.js
+++ models/index.js
@@ -5,12 +5,38 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  // TODO: Need to check to see if this foreign key will work given category has no foreign key set up in the model!!!!
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tag_list'
+});
 
 module.exports = {
   Product,
